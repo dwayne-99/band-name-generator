dff7e36c8106286b1cb5a0f60f3502fe2388ab14
dwayne-99 <dwaynephillips01@gmail.com>
2024-03-29 09:51:57 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Generate your personal Band Name")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet)
