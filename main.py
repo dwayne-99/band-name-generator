085c378de74625353df0ad780041608ded21476d
dwayne-99 <121464672+dwayne-99@users.noreply.github.com>
2024-04-12 20:14:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print("Generate your personal Band Name")
+# prints text to console
 
 city = input("What city did you grow up in?\n")
+# user inputs city, \n puts input on new line
 
 pet = input("What is the name of your pet?\n")
+# user inputs pet name
 
 print("Your band name could be " + city + " " + pet)
+# returns strings and given variables and prints the combined text to console
